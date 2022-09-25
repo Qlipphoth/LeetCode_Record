94b14f02b35984a00966ff12054ce9bd6154ede4
Qliphoth <1841362281@qq.com>
2022-09-25 15:16:06 +0800

    9.25 noon
    数位dp so hard

--- solution0001.py
+++ solution0001.py
@@ -1,23 +1,31 @@
-class Solution(object):
+from functools import cache
+class Solution:
+    def rotatedDigits(self, n: int) -> int:
+        check = [0, 0, 1, -1, -1, 1, 1, -1, 0, 1]
+        digits = [int(digit) for digit in str(n)]
 
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        length = len(nums)
-        for i in range(length):
-            b = target - nums[i]
-            for j in range(i+1, length):
-                if b == nums[j]:
-                    return [i, j]
-        #         else:
-        #             continue
-        # return [i, j]
+        @cache
+        def dfs(pos: int, bound: bool, diff: bool) -> int:
+            print(pos, bound, diff)
+            if pos == len(digits):
+                return int(diff)
+            
+            ret = 0
+            for i in range(0, (digits[pos] if bound else 9) + 1):
+                if check[i] != -1:
+                    ret += dfs(
+                        pos + 1,
+                        bound and i == digits[pos],
+                        diff or check[i] == 1
+                    )
+            
+            return ret
+            
+        
+        ans = dfs(0, True, False)
+        dfs.cache_clear()
+        return ans
 
 if __name__ == '__main__':
-    nums = [1, 2, 3, 4, 5]
-    target = 7
     s = Solution()
-    print(s.twoSum(nums, target))
+    s.rotatedDigits(15)
