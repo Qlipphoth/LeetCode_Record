60d560be4d3268078164df236902335d94a0dbd7
qlipphoth <1841362281@qq.com>
2024-07-05 10:00:47 +0800

    4数之和

--- C++Codes/codes/18.4-sum.cpp
+++ C++Codes/codes/18.4-sum.cpp
@@ -33,16 +33,19 @@ public:
         sort(nums.begin(), nums.end());
         for (int i = 0; i < len - 3; ++i) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i; j < len - 2; ++j) {
+            for (int j = i + 1; j < len - 2; ++j) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
                 int l = len - 1;
                 for (int k = j + 1; k < len - 1; ++k) {
                     if (k == l) break;
                     if (k > j + 1 && nums[k] == nums[k - 1]) continue;
-                    int cur = target - nums[i] - nums[j] - nums[k];
+                    long cur = (long)target - nums[i] - nums[j] - nums[k];
                     while (l > k + 1 && nums[l] > cur) --l;
                     if (nums[l] == cur) {
-                        if (l < len - 1 && nums[l] == nums[l + 1]) continue;
+                        if (l < len - 1 && nums[l] == nums[l + 1]) {
+                            -- l;
+                            continue;
+                        }
                         ans.push_back({nums[i], nums[j], nums[k], nums[l]});
                     }
                 }
