5cbf7e50fe50845bc4a6f0e0d8e5e8bc12969177
qlipphoth <1841362281@qq.com>
2024-07-19 19:06:01 +0800

    add codes

---
+++ C++Codes/codes/464.can-i-win.cpp
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=464 lang=cpp
+ * @lcpr version=30204
+ *
+ * [464] 我能赢吗
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    unordered_map<pair<bool, int>, bool> memo;
+
+    bool dfs()
+
+
+    bool canIWin(int maxChoosableInteger, int desiredTotal) {
+
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 10\n11\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 10\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 10\n1\n
+// @lcpr case=end
+
+ */
+
